a473d04c9c4fc567b9ce691fa545369f28fcdc1f
Bekzod Mirzaaliyev <88118057+Bekzodmirzaaliyev@users.noreply.github.com>
2021-12-08 19:48:35 +0500

    tg

---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "2105630825:AAGZUYjS_PREq1z3KU3PMOGxsYRsQUhI1D4";
+$chat_id = "-1001527109455";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
